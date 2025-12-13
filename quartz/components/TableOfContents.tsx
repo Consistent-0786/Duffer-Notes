31e607b19d739af0d95b05121ef44d9a975b5560
Consistent-0786 <kartartrading.01@gmail.com>
2025-12-13 18:35:32 +0530

    Update notes: 12/13/2025 18:35:32.ToUniversalTime().ToString('yyyy-MM-dd HH:mm:ss') UTC

--- quartz/components/TableOfContents.tsx
+++ quartz/components/TableOfContents.tsx
@@ -5,23 +5,40 @@ import { classNames } from "../util/lang"
 // @ts-ignore
 import script from "./scripts/toc.inline"
 
+/* Recursive TOC list renderer */
+function TocList({ items }: { items: any[] }) {
+  if (!items || items.length === 0) return null
+
+  return (
+    <ul class="toc-list">
+      {items.map((item) => (
+        <li key={item.slug} class={`depth-${item.depth - 1}`}>
+          <a href={`#${item.slug}`} data-for={item.slug}>
+            {item.text}
+          </a>
+
+          {item.children && item.children.length > 0 && (
+            <TocList items={item.children} />
+          )}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
 export default (() => {
   const TableOfContents: QuartzComponent = ({ fileData, displayClass, cfg }: QuartzComponentProps) => {
-    if (!fileData?.toc) return null
+    if (!fileData?.toc || fileData.toc.length === 0) return null
 
     return (
       <aside class={classNames(displayClass, "quartz-toc-sidebar")}>
         <nav class="toc-section" aria-label="Table of Contents">
-          <h3 class="toc-title">{i18n(cfg.locale).components.tableOfContents.title}</h3>
-          <ul class="toc-list">
-            {fileData.toc.map((tocEntry) => (
-              <li key={tocEntry.slug} class={`depth-${tocEntry.depth}`}>
-                <a href={`#${tocEntry.slug}`} data-for={tocEntry.slug}>
-                  {tocEntry.text}
-                </a>
-              </li>
-            ))}
-          </ul>
+          <h3 class="toc-title">
+            {i18n(cfg.locale).components.tableOfContents.title}
+          </h3>
+
+          {/* Render nested TOC */}
+          <TocList items={fileData.toc} />
         </nav>
       </aside>
     )
